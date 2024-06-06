3787b96674c163d8f5a1dd2feede1050b0bed286
shyeshye <shyehezkel2@gmail.com>
2024-06-06 15:17:24 +0300

    added login and sign in pages

---
+++ shahar/forntend/shye-project/src/login.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function Login(){
+    const [Email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = () => {
+        // Add authentication logic here
+        console.log('Username:      ', Email);
+        console.log('Password:', password);
+    };
+
+    return (
+        <div>
+            <h1>login</h1>
+            <form onSubmit={handleSubmit}>
+                <div className='form-login-user'>
+                    <label>Username: </label>
+                    <input
+                        type="text"
+                        placeholder='example@example.com'
+                        value={Email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <p></p>
+                <button type="submit">Login</button>
+                <div>
+                    <p>Don't already have an account? </p>
+                </div>
+                <button type="button" onClick={() => navigate('/dashboard')}>Sign In</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
---
+++ shahar/forntend/shye-project/src/signIn.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+function signIn(){
+  const [Email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+      // Add authentication logic here
+      console.log('Username:      ', Email);
+      console.log('Password:', password);
+  };
+
+  return (
+      <div>
+          <h1>login</h1>
+          <form onSubmit={handleSubmit}>
+              <div className='form-login-user'>
+                  <label>Username: </label>
+                  <input
+                      type="text"
+                      placeholder='example@example.com'
+                      value={Email}
+                      onChange={(e) => setEmail(e.target.value)}
+                  />
+              </div>
+              <div>
+                  <label>Password: </label>
+                  <input
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                  />
+              </div>
+              <p></p>
+              <button type="submit">Create Account</button>
+          </form>
+      </div>
+  );
+};
+
+export default signIn;
