04877d6560b8a4ddff3463ba53e69e785fbab613
Elazar <elazar@example.com>
2024-06-09 15:10:47 +0300

    some changes in sigm up and sign in components

--- elazar/frontend/vite-project/src/App.tsx
+++ elazar/frontend/vite-project/src/App.tsx
@@ -1,16 +1,37 @@
-import './App.css'
-import Login from './login'
-import Create_account from './create_account'
-
+import Home from "./Home";
+import SignIn from "./pages/SignIn";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
-
   return (
-    <>
-        <Login />
-        <Create_account />
-    </>
-  )
+    <div>
+      <Router>
+        <div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <h1>
+                  <NavLink to="/SignIn"> Press</NavLink>
+                </h1>
+              }
+            />
+            <Route path="/SignIn" element={<SignIn />} />
+            <Route path="/Home" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+/*
+ 
+*/
