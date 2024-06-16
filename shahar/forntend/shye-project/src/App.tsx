b2d48f5b1a20b61367fd791919ffefe2fd79dd9e
shyeshye <shyehezkel2@gmail.com>
2024-06-16 10:21:22 +0300

    new backend

--- shahar/forntend/shye-project/src/App.tsx
+++ shahar/forntend/shye-project/src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
-import Dashboard from './signUp';
 import Login from './login';
 import { Route, Routes } from 'react-router-dom';
 import Home from './home';
+import SignUp from './signUp';
 
 function App() {
   return (
     <Routes>
     <Route path="/" element={<Login />} />
-    <Route path="/signUp" element={<Dashboard/>} />
+    <Route path="/signup" element={<SignUp/>} />
     <Route path='/home' element={<Home/>} />
   </Routes>
   );
