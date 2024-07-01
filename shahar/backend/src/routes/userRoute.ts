026c0a80a5b9293fa5ba28c3f18850014e6ec6e8
shyeshye <shyehezkel2@gmail.com>
2024-07-01 14:15:02 +0300

    added home page after login

--- shahar/backend/src/routes/userRoute.ts
+++ shahar/backend/src/routes/userRoute.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { pool } from '../config/db'
 
 const router = express.Router();
 
@@ -7,10 +8,30 @@ router.post('/user', (req, res) => {
     res.send('create user');
 
   });
-router.get('/user', (req, res) => {
-    console.log('Get user');
-    res.json('Get user');
+router.get('/user', async (req, res) => {
+    const { email } = req.query;
+   
+  if (!email) {
+    return res.status(400).json({ success: false, message: 'Email is required' });
+  }
+
+  try {
+    const [rows] = await pool.query(
+      'SELECT * FROM s_training_db.users WHERE user_email = ?;',
+      [email]
+    );
+
+    if ((rows as any[]).length > 0) {
+      return res.json(rows);
+    } else {
+      return res.status(404).json({ success: false, message: 'No such user' });
+    }
+  } catch (err) {
+    return res.status(500).json({ success: false, message: 'Internal Server Error', error: (err as Error).message });
+  }
 });
+
+
 router.put('/user', (req, res) => {
     console.log('Update user');
     res.json('Update user');
